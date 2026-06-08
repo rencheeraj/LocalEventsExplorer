7f193cab8a9d685e7821d3bd8aaea26afdf4cc01
rencheeraj <rencheerajm@gmail.com>
2026-06-08 12:47:53 -0400

    chore: scaffold SwiftUI project

---
+++ LocalEventsExplorer/LocalEventsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalEventsExplorerApp.swift
+//  LocalEventsExplorer
+//
+//  Created by Rencheeraj Mohan on 2026-06-08.
+//
+
+import SwiftUI
+
+@main
+struct LocalEventsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
